7e61cbe42dfe67c4e113ca47303d7c7ae320a152
basitmaulana <abdulbasitmaulana00@gmail.com>
2026-09-18 18:39:39 +0700

    Update API keys and cron key in app.py
    Updated API keys and cron key in app.py.

--- app.py
+++ app.py
@@ -36,14 +36,14 @@ FLIGHTS_FILE = os.path.join(DATA_DIR, "flights.json")
 app = Flask(__name__)
 
 # ---------------- ISI BAGIAN INI ----------------
-AERODATABOX_API_KEY = "ISI_API_KEY_RAPIDAPI_DI_SINI"
+AERODATABOX_API_KEY = "44e5aa0765msh8f56735bdbcf1fcp1a2250jsn43d85ca9934c"
 AERODATABOX_HOST = "aerodatabox.p.rapidapi.com"
 
-VAPID_PUBLIC_KEY = "ISI_SETELAH_GENERATE_VAPID"
+VAPID_PUBLIC_KEY = "BBFSK1qtWbVTACECJenEE9rig7_MbekM9kkJwc7uW0o3ijqUXBDZPZb3t95qwfyjwbubic1EbULG5PKbT73dbuM"
 VAPID_PRIVATE_KEY_PATH = os.path.join(APP_DIR, "vapid_private.pem")
 VAPID_CLAIMS_SUB = "mailto:ganti@dengan-email-kamu.com"
 
-CRON_KUNCI = "ganti-dengan-kata-sandi-acak-punya-sendiri"
+CRON_KUNCI = "22102000"
 # --------------------------------------------------
 
 
